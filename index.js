467ac52cf025136d795c44509531208931847ad5
Sergey Chernyshev <sergey.chernyshev@gmail.com>
2022-09-02 13:59:20 -0400

    Use generic parent class

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 import { fileURLToPath } from "node:url";
 
-export default function (config) {
-  return {
-    src: "src/",
-    boilerplate: fileURLToPath(new URL("boilerplate/", import.meta.url)),
-    buildCommand: "npm run build",
-    buildOutputFolder: "dist/",
-    getDeployCommand: (deployment) => {
-      `echo "Deploying to ${deployment.host}"`;
-    },
-  };
+import { BorderlessEnvironment } from "@borderlessjs/borderless";
+
+export default class MainThreadEnvironment extends BorderlessEnvironment {
+  constructor(name, config) {
+    super(
+      name,
+      config,
+      "src",
+      "dist",
+      fileURLToPath(new URL("boilerplate/", import.meta.url)),
+      "npm run build"
+    );
+  }
+
+  getDeployCommand(deploymentConfig) {
+    `echo "Deploying static HTML to ${deploymentConfig.host}"`;
+  }
 }
